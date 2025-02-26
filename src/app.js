ee5112a65c044d6e8200975439b08cb4bf91a552
Suraj Verma <sv1009876@gmail.com>
2025-02-26 16:30:31 +0530

    Add user crud operation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@
 
  const app = express();
 
+ app.get("/user", (req, res)=> {
+    res.send({firstName: "Suraj",lastName: "Verma"});
+ })
+
+ app.post("/user", (req, res)=> {
+    console.log("User added");
+    res.send("User added successfully");
+ })
+
+ app.delete("/user", (req, res) => {
+    res.send("User deleted");
+ })
+
  app.use((req, res)  => {
     res.send("Hello from the server")
  })
