993e54c16f3431269ff9dc1d816c6e508942d482
Suraj Verma <sv1009876@gmail.com>
2025-02-28 15:39:55 +0530

    WIP

--- src/app.js
+++ src/app.js
@@ -3,14 +3,19 @@
  const database = require("./config/database");
  const User = require("./models/user"); 
 
+
+ // middleware to pass JSON data to javascript data.
+ app.use(express.json());
+
  app.post("/signup", async (req, res, next) => {
    const userObj = {
-      firstName: "Suraj",
-      lastName: "verma",
-      emailId: "suraj@yopmail.com",
-      age: 31,
-      gender: "Male"
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailId: req.body.emailId,
+      age: req.body.age,
+      gender: req.body.gender
    };
+   console.log("========== REQ.BODY ======", req.body);
    const user = new User(userObj);
    await user.save();
    res.status(201).json({
@@ -18,6 +23,22 @@
       user,
    })
  })
+
+//  GET:- User
+app.get("/user", async (req, res) => {
+   const email = req.body.email;
+   try {
+      const users = await User.find({ emailId: email});
+      if (users.length === 0) {
+         res.status(404).json({ message: "User not found"})
+      } else{
+         res.status(200).json({ users })
+      }
+   } catch (err){
+      res.status(400).send("Something went wrong")
+   }
+
+})
 
  database()
  .then(() => {
