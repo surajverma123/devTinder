ee574af2ae6509aa885fc540f418ecf55a573774
Suraj Verma <sv1009876@gmail.com>
2025-04-08 18:15:07 +0530

    Online & Offline code

--- src/models/user.js
+++ src/models/user.js
@@ -1,85 +1,100 @@
 const mongoose = require("mongoose");
-const validator = require('validator');
+const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            require: true,
-            minLength: 4,
-            maxLength: 30,
-        },
-        lastName: {
-            type: String,
-            minLength: 4,
-            maxLength: 30,
-        },
-        emailId: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-            lowercase: true,
-            validate(email) {
-                if (!validator.isEmail(email)){
-                    throw new Error("Invalid email address")
-                }
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            minLength: 4,
-            // maxLength: 30,
-        },
-        age: {
-            type: Number,
-            min: 18,
-        },
-        gender: {
-            type: String,
-            enum: {
-                values: ["male", "female", "other"],
-                message: `{VALUE} is not a valid gender type`
-            },
-        },
-        photoUrl: {
-            type: String,
-            default: "dummy url"
-        },
-        about: {
-            type: String,
-            default: "This is a default description for user"
-        },
-        skills: {
-            type: [String]
-        }
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minLength: 4,
+      maxLength: 30,
     },
-    { timestamps: true }
-)
-userSchema.index({ firstName: 1, lastName: 1 })
-userSchema.methods.getJWT = async function() {
-    const user = this;
-     const token = jwt.sign(
-            {
-              _id: user._id,
-              email: user.emailId,
-            },
-            "DEV@Tinder123#",
-            {
-              expiresIn: '1d'
-            }
-          );
-          
-    return token;
-}
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 30,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(email) {
+        if (!validator.isEmail(email)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 4,
+      // maxLength: 30,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "dummy url",
+    },
+    about: {
+      type: String,
+      default: "This is a default description for user",
+    },
+    skills: {
+      type: [String],
+    },
+    status: {
+      type: String,
+      default: 'offline',
+      enum: {
+        values: ['online', 'offline'],
+        message: `{VALUE} is not valid for status`,
+      }
+    },
+    lastSeen: {
+      type: mongoose.Schema.Types.Date,
+      default: null,
+    }
+  },
+  { timestamps: true }
+);
+userSchema.index({ firstName: 1, lastName: 1 });
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      email: user.emailId,
+    },
+    "DEV@Tinder123#",
+    {
+      expiresIn: "1d",
+    }
+  );
 
-userSchema.methods.validatePassword = async function(passwordInputByUser) {
-    const isPasswordValid = await bcrypt.compare(passwordInputByUser, this.password);
-    return isPasswordValid;
-}
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    this.password
+  );
+  return isPasswordValid;
+};
 
 const UserModel = mongoose.model("User", userSchema);
 module.exports = UserModel;
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,68 +1,100 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
-const Chat  = require("../models/chat");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const Chat = require("../models/chat");
 
 const getSecretRoomId = (userId, targetUserId) => {
-   return crypto.createHash('sha256')
-    .update([userId,targetUserId].sort().join("_"))
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
     .digest("hex");
-}
+};
 const initializeSocket = (server) => {
-    const io = socket(server, {
-        cors: {
-          origin: "http://localhost:5173",
-        },
-      });
-      
-      io.on('connection', (socket) => {
-        // Handle events
-        socket.on("joinChat", ({ userId, targetUserId, firstName }) => {
-            const room = getSecretRoomId(userId, targetUserId);
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+    },
+  });
 
-            socket.join(room)
-        });
+  io.on("connection", (socket) => {
+    // Handle events
+    socket.on("joinChat", async ({ userId, targetUserId, firstName }) => {
+      const room = getSecretRoomId(userId, targetUserId);
+      const user = await User.findOneAndUpdate(
+        { _id: userId },                         // filter
+        { status: "online", lastSeen: null },   // update
+        { new: true }                           // options: return updated document
+      );
+      // console.log("=========== USER ==========", user);
+      socket.join(room);
+    });
 
-        socket.on('sendMessage', async ({
+    socket.on(
+      "sendMessage",
+      async ({ firstName, lastName, userId, targetUserId, text }) => {
+        // Save message to the database
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+          // check the userId & targetUserId should be friends
+          const isFriend = ConnectionRequest.findOne({
+            $or: [
+              {
+                status: "accepted",
+                fromUserId: userId,
+                toUserId: targetUserId,
+              },
+              {
+                status: "accepted",
+                fromUserId: targetUserId,
+                toUserId: userId,
+              },
+            ],
+          });
+
+          if (!isFriend) {
+            return null;
+          }
+
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+          await chat.save();
+          io.to(roomId).emit("messageReceived", {
             firstName,
+            text,
             lastName,
-            userId,
-            targetUserId,
-            text
-        }) => {
-            const roomId = getSecretRoomId(userId, targetUserId) ;
-            console.log("======== TEXT =======", text);
-            console.log("======== TEXT userId =======", userId);
-            console.log("======== TEXT targetUserId =======", targetUserId);
-            // Save message to the database
-            try {
-                let chat = await Chat.findOne({
-                    participants: { $all: [userId, targetUserId]}
-                })
-               
-                if (!chat) {
-                    chat = new Chat({
-                        participants: [userId, targetUserId],
-                        messages:[]
-                    })
-                }
-                chat.messages.push({
-                    senderId: userId,
-                    text,
-                });
-                await chat.save();
-                io.to(roomId).emit("messageReceived", {
-                    firstName, text
-                })
-            } catch(error) {
-                console.error(error);
-            }
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    );
 
-        });
+    socket.on("user-disconnecting", async ({ userId }) => {
+      console.log("=========== USER ID =======", userId);
+      await User.findOneAndUpdate(
+        { _id: userId },                         // filter
+        { status: "offline", lastSeen: new Date() },   // update
+        { new: true }                           // options: return updated document
+      );
+    });
 
-        socket.on("disconnect", () => {
-
-        })
-      })
-}
+    socket.on("disconnect", async (userId ) => {
+      console.log("User dis-connected successfully");
+    });
+  });
+};
 
 module.exports = initializeSocket;
