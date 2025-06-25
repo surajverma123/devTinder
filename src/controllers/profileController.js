e8d97e21ffd4951ff6992c1e796131358b2239c5
Suraj Verma <sv1009876@gmail.com>
2025-06-25 14:51:45 +0530

    Add profileservice

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,21 +1,19 @@
 
 const { validateProfileEditData }  = require('../utils/validation');
-const User = require('../models/user');
+const { fetchProfile, updateUserProfile } = require('../services/profileService');
 
 const getProfile = async (req, res) => {
   try {
     const userId = req.userId;
-    if(!userId) {
-      throw new Error('User is not exits, please login again');
-    }
 
-    const user = await User.findById(userId);
+    const { user } = await fetchProfile({ userId });
+
     res.status(200).json({
       success: true,
       user,
     });
   } catch (error) {
-    res.status(403).json({
+    res.status(error.statusCode || 403).json({
       message: error.message || 'Something went wrong',
       error
     });
@@ -27,12 +25,9 @@ const updateProfile = async(req, res) => {
     if (!validateProfileEditData(req)) {
       throw new Error('Invalid Edit request');
     }
-    const { user: loggedInUser } = req;
-  
-    // Updating value with new values that provided by user
-    Object.keys(req.body).forEach((key) => loggedInUser[key] = req.body[key]);
-  
-    await loggedInUser.save();
+    const { user: loggedInUser, body  } = req;
+    await updateUserProfile({ body,  loggedInUser });
+   
     res.status(201).json({
       message: 'User updated successfully',
       user: loggedInUser 
