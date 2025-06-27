5a38d4bfb17ae29c348c54bffc20745bdccd92c9
Suraj Verma <sv1009876@gmail.com>
2025-06-27 13:53:00 +0530

    Add get favorite api

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,8 @@ const {
   requestReceived,
   profile,
   addToFavorite,
-  removeFromFavorite
+  removeFromFavorite,
+  getAllFavorites,
 } = require('../controllers/userController');
 
 
@@ -21,6 +22,7 @@ router.get('/connections', userAuth, connections);
 router.get('/feed', userAuth, feed);
 router.get('/profile/:userId', userAuth, profile);
 
+router.get('/favorite', userAuth, getAllFavorites);
 router.post('/favorite', userAuth, addToFavorite);
 
 router.delete('/favorite', userAuth, removeFromFavorite);
